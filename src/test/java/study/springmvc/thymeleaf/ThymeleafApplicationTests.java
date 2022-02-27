2ac31533168d4272ea000cc3d0ecce2e1d27e41d
hclee <heechul4296@gmail.com>
2022-02-28 00:15:00 +0900

    add project source for thymeleaf

---
+++ src/test/java/study/springmvc/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springmvc.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
