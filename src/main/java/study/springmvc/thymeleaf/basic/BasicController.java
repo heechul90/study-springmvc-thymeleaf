026293794e3b444a57ce7c323f04ebeca2a796b1
hclee <heechul4296@gmail.com>
2022-03-01 13:37:22 +0900

    URL 링크

--- src/main/java/study/springmvc/thymeleaf/basic/BasicController.java
+++ src/main/java/study/springmvc/thymeleaf/basic/BasicController.java
@@ -71,6 +71,13 @@ public class BasicController {
         return "basic/basic-date";
     }
 
+    @GetMapping(value = "/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
     @Data
     static class User {
         private String username;
