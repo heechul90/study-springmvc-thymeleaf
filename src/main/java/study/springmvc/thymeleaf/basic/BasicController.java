174bf84bc3612045eca80bbfe6ebfd17f9d9dedb
hclee <heechul4296@gmail.com>
2022-03-01 11:27:57 +0900

    텍스트 - text, utext

---
+++ src/main/java/study/springmvc/thymeleaf/basic/BasicController.java
@@ -0,0 +1,25 @@
+package study.springmvc.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/basic")
+public class BasicController {
+
+    @GetMapping(value = "/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "Hello Spring!!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping(value = "/text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "Hello <b>Spring!!</b>");
+        return "basic/text-unescaped";
+    }
+
+
+}
